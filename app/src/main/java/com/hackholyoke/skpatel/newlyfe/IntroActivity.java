bf2ef15475c446cb069365ed94fc0d9fa543cec5
Tyler Fameli <tylerfameli@gmail.com>
2016-11-04 22:42:07 -0400

    New Screen

--- app/src/main/java/com/hackholyoke/skpatel/newlyfe/IntroActivity.java
+++ app/src/main/java/com/hackholyoke/skpatel/newlyfe/IntroActivity.java
@@ -1,18 +1,14 @@
 package com.hackholyoke.skpatel.newlyfe;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -20,7 +16,7 @@ import java.util.Calendar;
 
 import static com.google.android.gms.appindexing.AppIndex.*;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
 
     /**
@@ -28,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), )
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
         /*
         ATTENTION: This was auto-generated to implement the App Indexing API.
         See https://g.co/AppIndexing/AndroidStudio for more information.
