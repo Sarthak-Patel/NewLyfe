172b6aad71e7db0c9d9619f12faf82a937984d04
Tyler Fameli <tylerfameli@gmail.com>
2016-11-05 03:28:07 -0400

    added speeches

--- app/src/main/java/com/hackholyoke/skpatel/newlyfe/ChoiceActivity.java
+++ app/src/main/java/com/hackholyoke/skpatel/newlyfe/ChoiceActivity.java
@@ -1,5 +1,7 @@
 package com.hackholyoke.skpatel.newlyfe;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,7 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class ChoiceActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +33,59 @@ public class ChoiceActivity extends AppCompatActivity {
         String Workout = "Workout";
         String Deadline = "Deadline";
         String Lost = "Lost";
-        if(state == 1) {
+        if (state == 1) {
             textView.setText(Deadline);
-        }
-        else if(state == 2) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.deadlinespeech);
+        } else if (state == 2) {
             textView.setText(Lost);
-        }
-        else if (state == 3) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.lostspeech);
+        } else if (state == 3) {
             textView.setText(Workout);
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.workoutspeech);
         }
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        mediaPlayer.start();
     }
 
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Choice Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        client.disconnect();
+    }
 }
