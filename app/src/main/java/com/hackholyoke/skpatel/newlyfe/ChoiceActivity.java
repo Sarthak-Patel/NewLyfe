780965fb7be0f51e84f379592fe323a8baf0730d
Tyler Fameli <tylerfameli@gmail.com>
2016-11-05 02:54:21 -0400

    three strings

--- app/src/main/java/com/hackholyoke/skpatel/newlyfe/ChoiceActivity.java
+++ app/src/main/java/com/hackholyoke/skpatel/newlyfe/ChoiceActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ChoiceActivity extends AppCompatActivity {
 
@@ -15,6 +16,19 @@ public class ChoiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choice);
         Bundle bundle = getIntent().getExtras();
         int state = bundle.getInt("State");
+        TextView textView = (TextView) findViewById(R.id.selectedText);
+        String Workout = "Workout";
+        String Deadline = "Deadline";
+        String Lost = "Lost";
+        if(state == 1) {
+            textView.setText(Workout);
+        }
+        else if(state == 2) {
+            textView.setText(Lost);
+        }
+        else if (state == 3) {
+            textView.setText(Workout);
+        }
     }
 
 }
