a77fcb614c40ba486acff535f4c99711d7eed3e2
Sarthak Patel <skpatel@umass.edu>
2016-11-04 21:12:23 -0400

    Second Commit-Testing commits

--- app/src/main/java/com/hackholyoke/skpatel/newlyfe/MainActivity.java
+++ app/src/main/java/com/hackholyoke/skpatel/newlyfe/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,7 +25,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        setupTaskButton();
     }
+
+    private void setupTaskButton() {
+        Button TaskButton = (Button) findViewById(R.id.btnTask);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
